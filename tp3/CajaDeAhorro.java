d3982e43d19a67570e03a640a0a8b63c98ca0a7f
ArielAlegre12 <arielalegre232@gmail.com>
2025-08-28 00:02:55 -0300

    Trabaje con el ejercicio 7

---
+++ tp3/CajaDeAhorro.java
@@ -0,0 +1,132 @@
+/**
+ * Represente una caja de ahorro
+ */
+public class CajaDeAhorro{
+    private int nroCuenta;
+    private double saldo;
+    private int extraccionesPosibles;
+    private Persona titular;
+    /**
+     * Constructor que inicializa la cuenta con número de cuenta y nombre de titular, el saldo incia en 0.
+     * @param p_nroCuenta es el num de la cuenta del titular
+     * @param p_titular de la cuenta
+     */
+    public CajaDeAhorro(int p_nroCuenta, Persona p_titular){
+        this.setNroCuenta(p_nroCuenta);
+        this.setTitular(p_titular);
+        this.saldo = 0;
+    }
+    /**
+     * Constructor que inicializa la cuenta ya con un saldo establecido
+     */
+    public CajaDeAhorro(int p_nroCuenta, Persona p_titular, double p_saldo){
+        this.setNroCuenta(p_nroCuenta);
+        this.setTitular(p_titular);
+        this.setSaldo(p_saldo);
+    }
+    /**
+     * Establece el número de la cuenta
+     * @param p_nroCuenta Num identificador.
+     */
+    private void setNroCuenta(int p_nroCuenta){
+        nroCuenta = p_nroCuenta;
+    }
+    /**
+     * Establece el títular de la cuenta
+     * @param p_titular persona dueña de la cuenta.
+     */
+    private void setTitular(Persona p_titular){
+        titular = p_titular;
+    }
+    /**
+     * Establece el saldo de la cuenta
+     * @param p_saldo es el que recibe por el usuario
+     */
+    private void setSaldo(double p_saldo){
+        saldo =  p_saldo;
+    }
+    /**
+     * establece la cantidad de extracciones que se pueden hacer
+     * @param p_extracciones es la cantidad de extracciones posibles
+     */
+    private void setExtraccionesPosibles(int p_extracciones){
+        extraccionesPosibles = p_extracciones;
+    }
+    /**
+     * devuelve el nro de cuenta
+     * @return la identificación de la cuenta
+     */
+    public int getNroCuenta(){
+        return nroCuenta;
+    }
+    /**
+     * devuelve el titular de la cuenta
+     * @return a la persona dueña de la cuenta
+     */
+    public Persona getTitular(){
+        return titular;
+    }
+    /**
+     * devuelve el saldo de la cuenta
+     * @return saldo 
+     */
+    public double getSaldo(){
+        return saldo;
+    }
+    /**
+     * devuelve la cantidad de extracciones permitidas
+     * @return cantidad de extracciones que se pueden hacer
+     */
+    public int getExtraccionesPosibles(){
+        return extraccionesPosibles;    
+    }
+    /**
+     * método que verifica si es posible hacer una extracción.
+     * devuelve true o false.
+     */
+    private boolean puedeExtraer(double p_importe){
+        if(p_importe <= getSaldo() && getExtraccionesPosibles() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /**
+     * método que analiza si puede extraer o no el dinero, si lo hace luego descuenta la cantidad de extracciones restantes.
+     * si no es posible, muestra porque no se pudo realizar la extracción.
+     */
+    public void extraer(double p_importe){
+        if(puedeExtraer(p_importe)){
+            extraccion(p_importe);
+            setExtraccionesPosibles(getExtraccionesPosibles() - 1);
+        }else if(p_importe > getSaldo()){
+            System.out.println("No puede extraer mas que el saldo!");
+        }else{
+            System.out.println("No tiene habilitadas mas extracciones!");
+        }
+    }
+    /**
+     * método que realiza la extracción.
+     */
+    private void extraccion(double p_importe){
+        p_importe = getSaldo() - p_importe;
+        setSaldo(p_importe);
+    }
+    /**
+     * método que realiza el deposito a la cuenta bancaria del importe recibido.
+     */
+    public void depositar(double p_importe){
+        p_importe += getSaldo();
+        setSaldo(p_importe);
+    }
+    /**
+     * método para visualizar los datos de la caja de ahorro del titular.
+     */
+    public void mostrar(){
+        System.out.println("Caja de Ahorro--");
+        System.out.println("Nro. Cuenta: " + getNroCuenta() + " - Saldo: " + getSaldo());
+        System.out.println("Titular: " + getTitular().nomYape());
+        System.out.println("Extracciones posibles: " + getExtraccionesPosibles());
+    }
+
+}
---
+++ tp3/CuentaCorriente.java
@@ -0,0 +1,135 @@
+/**
+ * Representa una cuenta corriente bancaria, con saldo, titular y límite de descubierto.
+ * Permite realizar depósitos y controlar extracciones según el saldo disponible.
+ */
+public class CuentaCorriente{
+    private int nroCuenta;
+    private double saldo;
+    private double limiteDescubierto;
+    private Persona titular;
+    
+    /**
+     * Constructor que inicializa la cuenta con número y titular.
+     * El saldo se establece en cero.
+     * @param p_nroCuenta Número identificador de la cuenta.
+     * @param p_titular Persona titular de la cuenta.
+     */
+    public CuentaCorriente(int p_nroCuenta, Persona p_titular){
+        this.setNroCuenta(p_nroCuenta);
+        this.setTitular(p_titular);
+        this.saldo = 0;
+    }
+    /**
+     * Constructor que inicializa la cuenta con número, titular y saldo inicial.
+     * @param p_nroCuenta Número identificador de la cuenta.
+     * @param p_titular Persona titular de la cuenta.
+     * @param p_saldo Saldo inicial de la cuenta.
+     */
+    public CuentaCorriente(int p_nroCuenta, Persona p_titular, double p_saldo){
+        this.setNroCuenta(p_nroCuenta);
+        this.setTitular(p_titular);
+        this.setSaldo(p_saldo);
+    }
+    /**
+     * Establece el número de cuenta.
+     * @param p_nroCuenta Número identificador.
+     */
+    private void setNroCuenta(int p_nroCuenta){
+        nroCuenta = p_nroCuenta;
+    }
+    /**
+     * Establece el titular de la cuenta.
+     * @param p_titular Persona dueña de la cuenta
+     */
+    private void setTitular(Persona p_titular){
+        titular = p_titular;
+    }
+    /**
+     * Establece el saldo de la cuenta.
+     * @param p_saldo saldo actual de la cuenta
+     */
+    private void setSaldo(double p_saldo){
+        saldo = p_saldo;
+    }
+    /**
+     * Establece el limite de descubierto de la cuenta
+     * @param p_limite es el permitido para retirar si la cuenta no posee el saldo suficiente
+     */
+    private void setLimiteDescubierto(double p_limite){
+        limiteDescubierto = p_limite;
+    }
+     /**
+      * Devuelve el número de cuenta.
+      * @return el número de cuenta del usuario.
+      */
+    public int getNroCuenta(){
+        return nroCuenta;
+    }
+    /**
+     * Devuelve el titular de la cuenta.
+     * @return Objeto Persona que representa al titular.
+     */
+    public Persona getTitular(){
+        return titular;
+    }
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     * @return Saldo disponible.
+     */
+    public double getSaldo(){
+        return saldo;
+    }
+    /**
+     * Devuelve el límite de descubierto permitido.
+     * @return Monto máximo permitido en negativo.
+     */
+    public double getLimiteDescubierto(){
+        return limiteDescubierto;
+    }
+   /**
+    * Realiza un depósito en la cuenta, sumando el importe al saldo actual.
+    * @param p_importe Monto a depositar.
+    */
+    public void depositar(double p_importe){
+        p_importe += getSaldo();
+        setSaldo(p_importe);
+    }
+    /**
+     * método que decide si puede o no retirar dinero 
+     * @return true o fasle
+     */
+    private boolean puedeExtraer(double p_importe){
+        if(p_importe <= getSaldo() + getLimiteDescubierto()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /**
+     * método que actua dependiendo si se puede o no retirar el dinero
+     * @param p_importe monto que desea extraer
+     */
+    public void extraer(double p_importe){
+        if(puedeExtraer(p_importe)){
+            extraccion(p_importe);
+        }else{
+            System.out.println("El importe de extraccion sobrepasa el límite de descubierto");
+        }
+    }
+    /**
+     * genera la extracción del dinero
+     */
+    private void extraccion(double p_importe){
+        p_importe = getSaldo() - p_importe;
+        setSaldo(p_importe);
+    }
+    /**
+     * visualiza los datos de la cuenta
+     */
+    public void mostrar(){
+        System.out.println("-   Cuenta Corriente –");
+        System.out.println("Nro. Cuenta: " + getNroCuenta() + " -  Saldo: " + getSaldo());
+        System.out.println("Titular: " + getTitular().nomYape());
+        System.out.println("Descubierto: " + getLimiteDescubierto());
+    }   
+}
